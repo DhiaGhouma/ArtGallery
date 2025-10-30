4ac91af361dabecfa347805ead26608560e75e92
Dhia Ghouma <ghoumadhia01@gmail.com>
2025-10-30 20:46:05 +0100

    image rendring bugfix

--- Backend/art_gallery/urls.py
+++ Backend/art_gallery/urls.py
@@ -1,16 +1,21 @@
-# art_gallery/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gallery.urls')),  # Include your gallery URLs
 ]
 
-# Serve media files in development
-if settings.DEBUG or True:  
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    
+# Serve media files - CRITICAL for production
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+    re_path(r'^static/(?P<path>.*)$', serve, {
+        'document_root': settings.STATIC_ROOT,
+    }),
+]
