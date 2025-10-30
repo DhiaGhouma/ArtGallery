d0e7cb2684baf84eea6030f1f11bbefdb623533a
Dhia Ghouma <ghoumadhia01@gmail.com>
2025-10-30 20:31:34 +0100

    Configure media file serving for production

--- Backend/art_gallery/urls.py
+++ Backend/art_gallery/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
 ]
 
 # Serve media files in development
-if settings.DEBUG:
+if settings.DEBUG or True:  
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
